2dbd4c8ff7c098ac9f2b651afe3e26e78c2b5e6e
Bilics Adrián <aaadrian@outlook.hu>
2019-08-03 10:46:08 +0200

    project structure

---
+++ Demo/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Assignment!");
+
+            Console.ReadKey();
+        }
+    }
+}
